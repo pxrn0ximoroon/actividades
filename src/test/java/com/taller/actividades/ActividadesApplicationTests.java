c83438e1cb49e2abeee1d5af0ec0ae93502c057b
Pxrn0 <anfeesra@gmail.com>
2026-05-01 13:17:30 -0500

    Añadir entidad actividad

---
+++ src/test/java/com/taller/actividades/ActividadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller.actividades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActividadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
