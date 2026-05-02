8ee70e4a0ffd386b994c8f1918400adfe37f58a0
juxhan05 <julsalazar15@gmail.com>
2026-05-02 14:31:41 -0500

    Update ActividadRepository.java

--- src/main/java/com/taller/actividades/repository/ActividadRepository.java
+++ src/main/java/com/taller/actividades/repository/ActividadRepository.java
@@ -4,7 +4,14 @@ import com.taller.actividades.model.Actividad;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
+/**
+ * Repositorio para acceder a los datos de las actividades en la base de datos.
+ * Hereda los métodos CRUD básicos de JpaRepository.
+ *
+ * @author Julian Andres - Andres Espinosa - Miguel David
+ * @version 1.0
+ * @since 2026
+ */
 @Repository
 public interface ActividadRepository extends JpaRepository<Actividad, Long> {
 }
